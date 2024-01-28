1e7b0d6414273308a9ea6587db6aec2fa50b2b04
yrmbeauty <maxim.konovodov@gmail.com>
2024-01-28 17:39:37 +0300

    Initial configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,42 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "prettier",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        arrowParens: "avoid",
+      },
+    ],
   },
-}
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, no-undef
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
